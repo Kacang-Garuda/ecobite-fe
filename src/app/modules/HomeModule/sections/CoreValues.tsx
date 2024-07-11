0f6190af4cd780b27db9329e363f2778c15e6d5d
mhmmdmadhanip <muhammad.madhani@ui.ac.id>
2024-07-12 05:42:19 +0700

    update: Core Value Hover Image

--- src/app/modules/HomeModule/sections/CoreValues.tsx
+++ src/app/modules/HomeModule/sections/CoreValues.tsx
@@ -6,26 +6,38 @@ const CoreValues = () => {
       <p className="font-bold text-[#2EAF7D] text-8xl max-w-lg flex self-center">Our Core Values</p>   
       <div className="relative  flex justify-center items-center group core-values-container">
         <img src="/images/home/sustainability.svg" alt="Sustainability Img" className="w-full  transition-opacity duration-700 group-hover:opacity-0" />
-        <div className="w-full absolute  transition-opacity duration-700 opacity-0 group-hover:opacity-100">
-          <p>Sustainability</p>
-          <p>Reduce food waste and promote enviromentally friendly practices</p>
+        <div className="w-full absolute  transition-opacity duration-700 opacity-0 group-hover:opacity-100 justify-center items-center flex flex-col px-8">
+          <p className='font-semibold text-3xl'>Sustainability</p>
+          <p className='text-center'>Reduce food waste and promote enviromentally friendly practices</p>
         </div>
       </div>
       <div className="relative  flex justify-center items-center group core-values-container">
         <img src="/images/home/volunteerism.svg" alt="Volunteerism Img" className="w-full justify-center items-center transition-opacity duration-700 group-hover:opacity-0" />
-        <img src="/images/home/volunteerism-hover.svg" alt="Volunteerism Hover Img" className="w-full  absolute transition-opacity duration-700 opacity-0 group-hover:opacity-100" />
+        <div className="w-full absolute  transition-opacity duration-700 opacity-0 group-hover:opacity-100 justify-center items-center flex flex-col px-8">
+          <p className='font-semibold text-3xl'>Volunteerism</p>
+          <p className='text-center'>Facilitate and celebrate community volunteer efforts.</p>
+        </div>
       </div>
       <div className="relative  flex justify-center items-center group core-values-container hover:pr-8 hover:pl-8 pr-16 ">
         <img src="/images/home/inclusivity.svg" alt="Inclusivity Img" className="w-full  transition-opacity duration-700 group-hover:opacity-0" />
-        <img src="/images/home/inclusivity-hover.svg" alt="Inclusivity Hover Img" className="w-full  absolute transition-opacity duration-700 opacity-0 group-hover:opacity-100" />
+        <div className="w-full absolute  transition-opacity duration-700 opacity-0 group-hover:opacity-100 justify-center items-center flex flex-col px-8">
+          <p className='font-semibold text-3xl'>Inclusivity</p>
+          <p className='text-center'>Promote respect, diversity, and inclusivity among users.</p>
+        </div>      
       </div>
       <div className="relative  flex justify-center items-center group core-values-container">
         <img src="/images/home/trust.svg" alt="Trust Img" className="w-full  transition-opacity duration-700 group-hover:opacity-0" />
-        <img src="/images/home/trust-hover.svg" alt="Trust Hover Img" className="w-full  transition-opacity absolute duration-700 opacity-0 group-hover:opacity-100" />
+        <div className="w-full absolute  transition-opacity duration-700 opacity-0 group-hover:opacity-100 justify-center items-center flex flex-col px-4">
+          <p className='font-semibold text-3xl'>Trust</p>
+          <p className='text-center'>Maintain safety, transparency, and accountability in all interactions.</p>
+        </div>      
       </div>
       <div className="relative  flex justify-center items-center group core-values-container px-16">
         <img src="/images/home/empowerment.svg" alt="Empowerment Img" className="w-full  transition-opacity duration-700 group-hover:opacity-0" />
-        <img src="/images/home/empowerment-hover.svg" alt="Empowerment Hover Img" className="w-full  transition-opacity absolute duration-700 opacity-0 group-hover:opacity-100" />
+        <div className="w-full absolute  transition-opacity duration-700 opacity-0 group-hover:opacity-100 justify-center items-center flex flex-col px-8">
+          <p className='font-semibold text-3xl'>Community Empowerment</p>
+          <p className='text-center'>Connect and support users through shared food and volunteer opportunities.</p>
+        </div>
       </div>
     </div>
   );
